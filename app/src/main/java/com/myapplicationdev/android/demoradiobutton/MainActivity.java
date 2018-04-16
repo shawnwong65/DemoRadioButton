dc1111851988d40655681cef65e087dbe3527146
shawnwong65 <16022774@myrp.edu.sg>
2018-04-16 15:12:07 +0800

    Added Radio Buttons and a Button

--- app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
@@ -2,6 +2,11 @@ package com.myapplicationdev.android.demoradiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+
+                Toast.makeText(getBaseContext(), rb.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
